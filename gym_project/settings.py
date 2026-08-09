4b3d9097713037639cb8df6b2f7632b4cee77695
Maria Shirokova <shirokova.marrie@outlook.com>
2026-08-09 15:16:22 +0300

    для деплоя

--- gym_project/settings.py
+++ gym_project/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-zoen5nmlc5ns@2^em+(l3k+^954@y=op8#g9a9q0q6i@&u)^aq'
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'django-insecure-zoen5nmlc5ns@2^em+(l3k+^954@y=op8#g9a9q0q6i@&u)^aq')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
